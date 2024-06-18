dcc7cc9988f5db120c0f22266749ce41883a4f40
Ara De Guzman <aradeguzman@aradesign.tech>
2024-06-17 17:07:13 -0700

    Initial commit

---
+++ app/src/main/java/com/mycompany/github_testing/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.mycompany.github_testing
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mycompany.github_testing.ui.theme.Github_TestingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Github_TestingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+
+    Column {
+        Button(onClick =  {} ) {
+            Text(text = "hi there")
+        }
+        Text(
+            text = "Hello how are  you ",
+            modifier = modifier
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Github_TestingTheme {
+        Greeting("Android")
+    }
+}
